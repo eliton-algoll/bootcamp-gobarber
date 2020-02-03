c95ef1a9c0cbbd4deb49149655b8442a4e4eb839
eliton-algoll <eliton@algoll.com.br>
2020-02-03 17:39:39 -0300

    add monitoramento de erros com o sentry

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
 import routes from './routes';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,11 +13,17 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // middleware que monitora os erros da aplicação
+    this.server.use(Sentry.Handlers.requestHandler());
+
     // configurando a aplicação para aceitar requisições em Json
     this.server.use(express.json());
     this.server.use(
@@ -23,6 +34,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d86fba1432994d0fbd3c01c47cf1011c@sentry.io/2218202',
+};
